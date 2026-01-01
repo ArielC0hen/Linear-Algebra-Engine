859ed9a44a2029c03d6997d09f8b439a63169f14
ArielC0hen <arielguyc@gmail.com>
2026-01-01 21:55:27 +0200

    'autosave'

--- Skeleton/src/test/java/testEngine.java
+++ Skeleton/src/test/java/testEngine.java
@@ -11,7 +11,7 @@ public class testEngine {
     }
 
     public static void testMatrixAddition() {
-        LinearAlgebraEngine engine = new LinearAlgebraEngine(3);
+        LinearAlgebraEngine engine = new LinearAlgebraEngine(1);
 
         System.out.println("---Check 1 (addition)---");
 
