2705dda745549873fd9610b80cb91b3e3fd495c7
ArielC0hen <arielguyc@gmail.com>
2026-01-01 19:25:14 +0200

    'autosave'

--- Skeleton/src/main/java/scheduling/TiredThread.java
+++ Skeleton/src/main/java/scheduling/TiredThread.java
@@ -95,7 +95,6 @@ public class TiredThread extends Thread implements Comparable<TiredThread> {
                 long usedStart = System.nanoTime();
                 task.run();
                 long usedEnd = System.nanoTime();
-                System.out.println("updated time used");
                 timeUsed.getAndAdd(usedEnd - usedStart);                
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
