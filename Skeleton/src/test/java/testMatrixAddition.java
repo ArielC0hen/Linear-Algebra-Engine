5c232b4eecbd78ae38774a95c3d4ee9a42e2617f
ArielC0hen <arielguyc@gmail.com>
2026-01-01 15:47:08 +0200

    'autosave'

--- Skeleton/src/test/java/testMatrixAddition.java
+++ Skeleton/src/test/java/testMatrixAddition.java
@@ -14,6 +14,8 @@ public class testMatrixAddition {
         dotTester();
         vecMultTester();
         //////////////////
+        getOrientationTester();
+        getTester();
         readRowMajorTester();
         loadRowMajorTester();
         loadColumnMajorTester();
