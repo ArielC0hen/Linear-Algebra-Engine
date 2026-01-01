2715226e7d1adb3af836643b62f11dafef80a285
ArielC0hen <arielguyc@gmail.com>
2026-01-01 20:00:17 +0200

    'autosave'

--- Skeleton/src/test/java/threadingTests.java
+++ Skeleton/src/test/java/threadingTests.java
@@ -25,7 +25,7 @@ public class threadingTests {
     public void testShutdownInterruption() throws InterruptedException {
     TiredExecutor executor = new TiredExecutor(2);
     
-    Thread shutdownThread = new Thread(() -> {
+    tiredThread shutdownThread = new Thread(() -> {
         try {
             executor.shutdown();
         } catch (InterruptedException e) {
