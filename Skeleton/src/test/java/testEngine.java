af9a2161c8d8759600b3659f5197ec449b7dfed0
ArielC0hen <arielguyc@gmail.com>
2026-01-01 21:13:13 +0200

    'autosave'

--- Skeleton/src/test/java/testEngine.java
+++ Skeleton/src/test/java/testEngine.java
@@ -1,8 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import memory.SharedMatrix;
+import parser.ComputationNode;
+import parser.ComputationNodeType;
 import scheduling.TiredExecutor;
 import scheduling.TiredThread;
+import spl.lae.LinearAlgebraEngine;
 
 public class testEngine {
     
@@ -14,6 +21,33 @@ public class testEngine {
         /////////////  
         submitAllTester();
     }
+
+    public void testSimpleAddition() {
+    LinearAlgebraEngine engine = new LinearAlgebraEngine(2);
+
+    // Create Matrix A: [[1, 2], [3, 4]]
+    double[][] dataA = {{1.0, 2.0}, {3.0, 4.0}};
+    SharedMatrix matrixA = new SharedMatrix(dataA);
+
+    // Create Matrix B: [[5, 6], [7, 8]]
+    double[][] dataB = {{5.0, 6.0}, {7.0, 8.0}};
+    SharedMatrix matrixB = new SharedMatrix(dataB);
+
+    // Build the tree
+    ComputationNode nodeA = new ComputationNode(ComputationNodeType.MATRIX, matrixA);
+    ComputationNode nodeB = new ComputationNode(ComputationNodeType.MATRIX, matrixB);
+    ComputationNode addNode = new ComputationNode(ComputationNodeType.ADD);
+    addNode.addChild(nodeA);
+    addNode.addChild(nodeB);
+
+    // Run
+    ComputationResult result = engine.loadAndCompute(addNode);
+    SharedMatrix resultMat = result.getMatrix();
+
+    // Verify: Result should be [[6, 8], [10, 12]]
+    assertEquals(6.0, resultMat.get(0).get(0));
+    assertEquals(12.0, resultMat.get(1).get(1));
+}
     
 
     public static void submitAllTester() throws InterruptedException {
