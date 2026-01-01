f11dc1110b68ae9327dc71d28d080cc500b1db8a
ArielC0hen <arielguyc@gmail.com>
2026-01-01 14:40:34 +0200

    'autosave'

--- Skeleton/src/test/java/testMatrixAddition.java
+++ Skeleton/src/test/java/testMatrixAddition.java
@@ -6,7 +6,7 @@ public class testMatrixAddition {
 
     public static void main(String[] args) {
         System.out.println("Checking Vector and Matrix functions!!!!!");
-        tranposeTest();
+        tranposeTester();
         addTester();
         dotTester();
         vecMultTester();
