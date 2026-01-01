2d817c2d91cdd77aa076494bf693ff9d46e22577
ArielC0hen <arielguyc@gmail.com>
2026-01-01 14:05:24 +0200

    'autosave'

--- Skeleton/src/test/java/testMatrixAddition.java
+++ Skeleton/src/test/java/testMatrixAddition.java
@@ -17,8 +17,9 @@ public class testMatrixAddition {
         System.out.println("---Check 1 (Row x Column Matrix)---");
         double[] c1v1 = {3.0, 6.0, 2.0};
         SharedVector c1sv1 = new SharedVector(c1v1, VectorOrientation.ROW_MAJOR); 
-        double[][] c1M = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
-        SharedMatrix c1M = new SharedMatrix(); 
+        double[][] c1m = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
+        SharedMatrix c1sm = new SharedMatrix(c1m);
+        c1sv1.vecMatMul(c1sm);
     }
 
     public static void dotTester() {
