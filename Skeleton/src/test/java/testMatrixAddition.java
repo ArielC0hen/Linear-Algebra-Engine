51bff28a19434b4faa27bfe60262f9ff13d8fa30
ArielC0hen <arielguyc@gmail.com>
2026-01-01 13:55:28 +0200

    'autosave'

--- Skeleton/src/test/java/testMatrixAddition.java
+++ Skeleton/src/test/java/testMatrixAddition.java
@@ -11,7 +11,7 @@ public class testMatrixAddition {
     }
 
     public static void vecMultTester() {
-        System.out.println("Checking the dot function in SharedVector");
+        System.out.println("Checking the vecMult function in SharedVector");
     }
 
     public static void dotTester() {
