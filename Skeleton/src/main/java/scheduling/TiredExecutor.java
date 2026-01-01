56d227af1e9a0b0e2c1e0c67b03e51054ed74359
ArielC0hen <arielguyc@gmail.com>
2026-01-01 23:24:19 +0200

    'autosave'

--- Skeleton/src/main/java/scheduling/TiredExecutor.java
+++ Skeleton/src/main/java/scheduling/TiredExecutor.java
@@ -49,26 +49,14 @@ public class TiredExecutor {
         for (Runnable task : tasks) {
             submit(task);
         }
-        synchronized (inFlight) {
-            while (inFlight.get() > 0) {
-                try {
-                    inFlight.wait(); // efficient wait until notified
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    break;
-                }
-            }
-        }
-        /* 
         while (inFlight.get() > 0) {  // haven't finished executing all yet 
             try {
-                Thread.sleep(5);
+                Thread.sleep(5); // we know busy wait isnt ideal but synchoronize
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 break;
             }
         }
-            */
     }
 
     public void shutdown() throws InterruptedException {
