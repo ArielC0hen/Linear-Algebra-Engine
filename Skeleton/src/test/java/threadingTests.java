f113f68a9772c1ed46fda1ac913ca5a688919cdb
ArielC0hen <arielguyc@gmail.com>
2026-01-01 19:08:45 +0200

    'autosave'

--- Skeleton/src/test/java/threadingTests.java
+++ Skeleton/src/test/java/threadingTests.java
@@ -1,6 +1,3 @@
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import scheduling.TiredThread;
 
 public class threadingTests {
