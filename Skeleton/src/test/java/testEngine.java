8918a4d86008aa58a634fd3e0a1f327f2dd45860
ArielC0hen <arielguyc@gmail.com>
2026-01-01 22:03:46 +0200

    'autosave'

--- Skeleton/src/test/java/testEngine.java
+++ Skeleton/src/test/java/testEngine.java
@@ -8,7 +8,7 @@ public class testEngine {
     
     public static void main(String[] args) throws InterruptedException {
         testMatrixAddition();
-        testMatrixMultiplication();
+        //testMatrixMultiplication();
     }
 
     public static void testMatrixMultiplication() {
