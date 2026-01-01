be740d1519b2cb342478275f1a0f4462a1e3a3f0
ArielC0hen <arielguyc@gmail.com>
2026-01-01 14:05:30 +0200

    'autosave'

--- Skeleton/src/test/java/testMatrixAddition.java
+++ Skeleton/src/test/java/testMatrixAddition.java
@@ -19,6 +19,7 @@ public class testMatrixAddition {
         SharedVector c1sv1 = new SharedVector(c1v1, VectorOrientation.ROW_MAJOR); 
         double[][] c1m = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
         SharedMatrix c1sm = new SharedMatrix(c1m);
+        
         c1sv1.vecMatMul(c1sm);
     }
 
