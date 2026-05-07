28e9cafd6ba0f649026594179966ba1132abf523
ArielC0hen <arielguyc@gmail.com>
2026-05-08 00:07:27 +0300

    'autosave'

--- ppl/2/ex2/L3/L3-ast.ts
+++ ppl/2/ex2/L3/L3-ast.ts
@@ -267,7 +267,8 @@ const parseClassExp = (fields: Sexp, methods: Sexp[]): Result<ClassExp> => {
     if (!isGoodBindings(methods)) {
         return makeFailure('Malformed methodss in "class" expression');
     } 
-    const methodNames = map(b => b[0],methods); // string[]
+    const methodNames = map(first,methods) as string[]; // string[]
+    //const methodNames = map(b => b[0],methods); // string[]
     const valsResult = mapResult(parseL3CExp, map(second, methods)); // Result<CExp[]>
     const methodsResult = mapv(valsResult, (vals: CExp[]) => zipWith(makeBinding, methodNames, vals)); // Result<Binding[]>
     return mapv(methodsResult,
