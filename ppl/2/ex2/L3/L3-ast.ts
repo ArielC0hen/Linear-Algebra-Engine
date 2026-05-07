e4de3077c7a06eba2b9e995856e2690192147eb4
ArielC0hen <arielguyc@gmail.com>
2026-05-07 19:47:44 +0300

    'autosave'

--- ppl/2/ex2/L3/L3-ast.ts
+++ ppl/2/ex2/L3/L3-ast.ts
@@ -178,7 +178,8 @@ export const parseL3SpecialForm = (op: Sexp, params: Sexp[]): Result<CExp> =>
     op === "quote" ? 
         isNonEmptyList<Sexp>(params) ? parseLitExp(first(params)) :
         makeFailure(`Bad quote exp: ${params}`) :
-    op === "class" ? :
+    op === "class" ? 
+    :
     makeFailure("Never");
 
 // DefineExp -> (define <varDecl> <CExp>)
