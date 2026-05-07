ff9c1e715dd4496c086ea1fc922726cae3e4e895
ArielC0hen <arielguyc@gmail.com>
2026-05-07 21:17:22 +0300

    'autosave'

--- ppl/2/ex2/L3/L3-ast.ts
+++ ppl/2/ex2/L3/L3-ast.ts
@@ -332,7 +332,7 @@ const unparseLetExp = (le: LetExp) : string =>
     `(let (${map((b: Binding) => `(${b.var.var} ${unparseL3(b.val)})`, le.bindings).join(" ")}) ${unparseLExps(le.body)})`
 
 const unparseClassExp = (ce: ClassExp) : string => 
-    `(class (${map((p: VarDecl) => p.var, ce.fields).join(" ")}) ${unparseLExps(ce.body)})`
+    `(class (${map((p: VarDecl) => p.var, ce.fields).join(" ")}) (${map((b: Binding) => `(${b.var.var} ${unparseL3(b.val)})`, ce.methods).join(" ")})`
 
 export const unparseL3 = (exp: Program | Exp): string =>
     isBoolExp(exp) ? valueToString(exp.val) :
