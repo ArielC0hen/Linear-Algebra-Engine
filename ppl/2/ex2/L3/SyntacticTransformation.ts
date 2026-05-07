23a89c60af65fdc8a136258bce6ecad1be6b6d0d
ArielC0hen <arielguyc@gmail.com>
2026-05-07 23:53:54 +0300

    'autosave'

--- ppl/2/ex2/L3/SyntacticTransformation.ts
+++ ppl/2/ex2/L3/SyntacticTransformation.ts
@@ -17,7 +17,7 @@ export const class2proc = (exp: ClassExp): ProcExp => {
                 makePrimOp("eq?"),
                 [
                     makeVarRef("msg"),
-                    makeLitExp(makeSymbolSExp(methods[0].var.var))
+                    makeLitExp(makeSymbolSExp(methods[0].var.var)) // binding -var->
                 ]
             ),
             methods[0].val,
