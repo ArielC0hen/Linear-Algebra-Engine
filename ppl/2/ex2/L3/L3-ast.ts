b31df78507f89fb6791bee79a2da788939d09a73
ArielC0hen <arielguyc@gmail.com>
2026-05-07 20:27:53 +0300

    'autosave'

--- ppl/2/ex2/L3/L3-ast.ts
+++ ppl/2/ex2/L3/L3-ast.ts
@@ -261,13 +261,10 @@ const parseLetExp = (bindings: Sexp, body: Sexp[]): Result<LetExp> => {
 }
 
 const parseClassExp = (vars: Sexp, body: Sexp[]): Result<ClassExp> => {
-    if (!(isArray(vars) && allT(isString, vars))) {
-        return 
-    }
     isArray(vars) && allT(isString, vars) 
         ? makeClassExp(map(makeVarDecl, vars), map(makeBinding, body))
 
-        
+
         : makeFailure(`Invalid vars for ClassExp ${format(vars)}`);
 }
 
