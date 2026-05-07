eed42ee4166c53c4303333e920199a5fe378962d
ArielC0hen <arielguyc@gmail.com>
2026-05-07 20:43:27 +0300

    'autosave'

--- ppl/2/ex2/L3/L3-ast.ts
+++ ppl/2/ex2/L3/L3-ast.ts
@@ -272,7 +272,7 @@ const parseClassExp = (vars: Sexp, methods: Sexp[]): Result<ClassExp> => {
     const valsResult = mapResult(parseL3CExp, map(second, methods));
     const methodsResult = mapv(valsResult, (vals: CExp[]) => zipWith(makeBinding, methodNames, vals));
     return mapv(methodsResult,
-        ()
+        (methods: Binding[])
     );
 }
 
