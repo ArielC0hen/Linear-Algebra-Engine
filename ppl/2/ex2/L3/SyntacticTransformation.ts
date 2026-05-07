96331d0788c04af81182f253fad1f4243221883c
ArielC0hen <arielguyc@gmail.com>
2026-05-08 02:20:47 +0300

    'autosave'

--- ppl/2/ex2/L3/SyntacticTransformation.ts
+++ ppl/2/ex2/L3/SyntacticTransformation.ts
@@ -56,8 +56,7 @@ export const transform = (exp: Exp | Program): Result<Exp | Program> => {
     if (isIfExp(exp)) {
         return mapv(
             mapResult(transform, [exp.test, exp.then, exp.alt]),
-            ([test, then, alt]) =>
-                makeOk(makeIfExp(test,then,alt))
+            ([test, then, alt]) => makeIfExp(test,then,alt)
         );
     }
 }
