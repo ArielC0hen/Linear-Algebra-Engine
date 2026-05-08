fb5ec9c22fe82ed8ecd36fd84b72a8a4004674e0
ArielC0hen <arielguyc@gmail.com>
2026-05-08 19:57:36 +0300

    'autosave'

--- ppl/2/ex2/L3/SyntacticTransformation.ts
+++ ppl/2/ex2/L3/SyntacticTransformation.ts
@@ -78,6 +78,7 @@ export const transform = (exp: Exp | Program): Result<Exp | Program> => {
         );
     }
     if (isLetExp(exp)) {
+        const vars = map(b => b[0] as string, exp.bindings);
         const bindingNames = map(()exp.bindings)
         const newVals = mapv(
             exp.bindings.map(b=> b.val),
