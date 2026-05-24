b56f3b2577ead857da28281f744d1e036bcf87c4
ArielC0hen <arielguyc@gmail.com>
2026-05-24 18:28:14 +0300

    'autosave'

--- ppl/2/ex2/q3.ts
+++ ppl/2/ex2/q3.ts
@@ -1,4 +1,4 @@
-import { CExp, Exp, isNumExp, Program } from './L3/L3-ast';
+import { CExp, Exp, isBoolExp, isNumExp, Program } from './L3/L3-ast';
 import { Result, makeFailure, makeOk} from './shared/result';
 
 /*
@@ -15,7 +15,7 @@ const CExpToPython = (exp: CExp) : Result<string> => {
     if (isNumExp(exp)) {
         return makeOk(exp.val.toString());
     } 
-    else if ()
+    else if (isBoolExp(exp))
 }
 export const l2ToPython = (exp: Exp | Program): Result<string>  => 
     makeFailure("TODO");
