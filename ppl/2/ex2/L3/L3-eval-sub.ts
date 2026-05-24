4eee594eeee9083fbcf3ca755a7a02dcb6cf47c8
ArielC0hen <arielguyc@gmail.com>
2026-05-24 12:29:32 +0300

    'autosave'

--- ppl/2/ex2/L3/L3-eval-sub.ts
+++ ppl/2/ex2/L3/L3-eval-sub.ts
@@ -48,6 +48,11 @@ const evalIf = (exp: IfExp, env: Env): Result<Value> =>
         isTrueValue(test) ? L3applicativeEval(exp.then, env) : 
         L3applicativeEval(exp.alt, env));
 
+const evalClass = (exp: IfExp, env: Env): Result<Value> =>
+    bind(L3applicativeEval(exp.test, env), (test: Value) => 
+        isTrueValue(test) ? L3applicativeEval(exp.then, env) : 
+        L3applicativeEval(exp.alt, env));
+
 const evalProc = (exp: ProcExp, env: Env): Result<Closure> =>
     makeOk(makeClosure(exp.args, exp.body));
 
