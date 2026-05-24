3202c517f87e535532a39e99c0f0da4a2add3b19
ArielC0hen <arielguyc@gmail.com>
2026-05-24 18:33:11 +0300

    'autosave'

--- ppl/2/ex2/q3.ts
+++ ppl/2/ex2/q3.ts
@@ -22,7 +22,10 @@ const CExpToPython = (exp: CExp) : Result<string> => {
             return makeOk("False");
         }
     }
-    else if (isStrExp)
+    else if (isStrExp) {
+
+    }
+    return "";
 }
 export const l2ToPython = (exp: Exp | Program): Result<string>  => 
     makeFailure("TODO");
