6c84203bef9dc2d724cdbc4dcbf5cd5cadedc42c
ArielC0hen <arielguyc@gmail.com>
2026-05-24 19:13:43 +0300

    'autosave'

--- ppl/2/ex2/q3.ts
+++ ppl/2/ex2/q3.ts
@@ -66,7 +66,7 @@ const CExpToPython = (exp: CExp) : Result<string> => {
                             return makeOk(`(not ${randsStrs[0]})`);
                         }
                         if (op === "number?" || op === "boolean") {
-                            const toString = randsStrs.join(`)
+                            const toString = randsStrs.join(`${convertPrimOp(op)})
                             return makeOk(`(${ra}))
                         }
                     }
