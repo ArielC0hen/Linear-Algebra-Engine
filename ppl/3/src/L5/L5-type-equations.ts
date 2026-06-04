ada2ac76ac4733ce5cb2766762600ab5927465eb
ArielC0hen <arielguyc@gmail.com>
2026-06-04 23:38:16 +0300

    'autosave'

--- ppl/3/src/L5/L5-type-equations.ts
+++ ppl/3/src/L5/L5-type-equations.ts
@@ -86,11 +86,13 @@ export const expToPool = (exp: A.Exp): Pool => {
             const combinedPool = findVars(headLit, poolTail);
             return extendPool(e, combinedPool);
         }) () :
+        /* FIX
 A.isAppExp(e) ? extendPool(e,
                  reducePool(findVars,
                             cons(e.rator, e.rands),
                             pool)) :
         extendPool(e, pool);
+        */
     return findVars(exp, makeEmptyPool());
 };
 
