fdedd3175d55846f498e5d92737f3c0cc6af822f
ArielC0hen <arielguyc@gmail.com>
2026-06-04 13:52:09 +0300

    'autosave'

--- ppl/3/src/L5/L5-typecheck.ts
+++ ppl/3/src/L5/L5-typecheck.ts
@@ -249,11 +249,8 @@ export const typeofProgramExps = (exps: List<Exp>, tenv: TEnv): Result<TExp> => 
     return bind (
         firstTE,
         () => {
-            // define, extend tenv
-            if (isDefineExp(firstExp)) {
-                const newEnv = makeExtendTEnv([firstExp.var.var],[firstExp.var.texp], tenv);
-                return typeofProgramExps(restExps, newEnv);
-            }
+            const newEnv = 
+            isDefineExp(firstExp)
             return typeofProgramExps(restExps, tenv);
         }
     );
