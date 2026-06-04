e7530815f67246190ce209a14fd3991d3c658d6d
ArielC0hen <arielguyc@gmail.com>
2026-06-04 12:56:43 +0300

    'autosave'

--- ppl/3/src/L5/L5-typecheck.ts
+++ ppl/3/src/L5/L5-typecheck.ts
@@ -224,10 +224,7 @@ export const typeofDefine = (exp: DefineExp, tenv: TEnv): Result<VoidTExp> => {
 
 
     return bind(
-        varTE,
-        (varTE : TExp) => bind (
-            
-        )
+        Equa
     )
 
     const testTE = typeofExp(ifExp.test, tenv);
