f0055b0b080bb7558ea5bc80afaaf0d0d5c61f43
ArielC0hen <arielguyc@gmail.com>
2026-06-04 19:39:51 +0300

    'autosave'

--- ppl/3/src/L5/L5-type-equations.ts
+++ ppl/3/src/L5/L5-type-equations.ts
@@ -78,7 +78,7 @@ export const expToPool = (exp: A.Exp): Pool => {
         A.isAtomicExp(e) ? extendPool(e, pool) :
         A.isProcExp(e) ? extendPool(e, reducePool(findVars, e.body, reducePoolVarDecls(extendPoolVarDecl, e.args, pool))) :
         A.isLitExp(e) && V.isEmptySExp(e.val) ?
-            extendPool(e, pool) :
+            pool : // HW3 3.3.a - fix this branch
         A.isLitExp(e) && V.isCompoundSExp(e.val) ?
             pool : // HW3 3.3.a - fix this branch
         A.isCompoundExp(e) ? extendPool(e, reducePool(findVars, A.expComponents(e), pool)) :
