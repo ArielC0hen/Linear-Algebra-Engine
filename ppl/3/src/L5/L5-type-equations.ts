43f95b9072b4c5906f3fa1b8d3cb297de57b253b
ArielC0hen <arielguyc@gmail.com>
2026-06-04 23:23:37 +0300

    'autosave'

--- ppl/3/src/L5/L5-type-equations.ts
+++ ppl/3/src/L5/L5-type-equations.ts
@@ -172,7 +172,7 @@ export const makeEquationsFromExp = (exp: A.Exp, pool: Pool): Opt.Optional<Equat
             [ makeEquation(left, T.makeBoolTExp()) ]) :
         isString(exp.val) ? Opt.mapv(inPool(pool, exp) , (left: T.TExp) =>
             [ makeEquation(left, T.makeStrTExp()) ]) :
-        Opt.makeNone()
+        Opt.mapv(inPool(pool, exp), (left: T.TExp) => [])
     ) :
     // The type of a number is Number
     A.isNumExp(exp) ? Opt.mapv(inPool(pool, exp), (left: T.TExp) => [makeEquation(left, T.makeNumTExp())]) :
