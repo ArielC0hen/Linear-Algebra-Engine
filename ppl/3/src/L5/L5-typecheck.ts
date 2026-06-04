7222cbc3316bce407e7993ced727a0e50bf6116e
ArielC0hen <arielguyc@gmail.com>
2026-06-04 13:41:00 +0300

    'autosave'

--- ppl/3/src/L5/L5-typecheck.ts
+++ ppl/3/src/L5/L5-typecheck.ts
@@ -249,7 +249,7 @@ export const typeofProgram = (exp: Program, tenv: TEnv): Result<TExp> => {
 //helper
 export const typeofProgramExps = (exps: List<Exp>, tenv: TEnv): Result<TExp> => {
     const firstExp = exps[0];
-    const firstTE = typeof()
+    const firstTE = typeofExp(firstExp, tenv);
     const restExps = exps.slice(1);
     if (restExps.length == 0) { // last expression
         return typeofExp(firstExp, tenv);
