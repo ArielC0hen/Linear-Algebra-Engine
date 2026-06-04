bb2e640f673fc0972d98679c9a8b5e7495008fc7
ArielC0hen <arielguyc@gmail.com>
2026-06-04 12:49:08 +0300

    'autosave'

--- ppl/3/src/L5/L5-typecheck.ts
+++ ppl/3/src/L5/L5-typecheck.ts
@@ -218,7 +218,7 @@ export const typeofLetrec = (exp: LetrecExp, tenv: TEnv): Result<TExp> => {
 //   If typeof(exp.val, tenv) = texp
 //   Then typeof(exp) = void
 export const typeofDefine = (exp: DefineExp, tenv: TEnv): Result<VoidTExp> => {
-    const varTE = typeofExp(exp.var, tenv);
+    const varTE = typeofExp(exp.var.texp, tenv);
     const AreEqual = checkEqualType()
 
     const testTE = typeofExp(ifExp.test, tenv);
