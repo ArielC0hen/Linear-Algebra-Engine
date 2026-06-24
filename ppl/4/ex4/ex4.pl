af72b62b0316a8d79abe36024561a03ddf5ea514
ArielC0hen <arielguyc@gmail.com>
2026-06-24 23:15:04 +0300

    'autosave'

--- ppl/4/ex4/ex4.pl
+++ ppl/4/ex4/ex4.pl
@@ -25,7 +25,7 @@ suffix(Xs, Ys) :- append(_Zs, Xs, Ys).
 % Signature: sub_list(Sublist, List)/2
 % Purpose: All elements in Sublist appear in List in the same order.
 % Precondition: List is fully instantiated (queries do not include variables in their second argument).
-sub_list(Xs,Xs)
+sub_list(Xs,Xs).
 sub_list(Xs,Ys) :- append(Xs,_As,_Bs), suffix(_Bs,Ys)
 
 
