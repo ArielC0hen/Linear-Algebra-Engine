7ba24e133538f2e0f8de78cacb23d4d881bf1c8b
ArielC0hen <arielguyc@gmail.com>
2026-06-25 00:55:44 +0300

    'autosave'

--- ppl/4/ex4/ex4.pl
+++ ppl/4/ex4/ex4.pl
@@ -59,8 +59,9 @@ swap_list1([X|Xs], As, Ys) :- swap_list1(Xs, [X|As], Ys).
 
 % Signature: sub_tree(Subtree, Tree)/2
 % Purpose: Tree contains Subtree.
-sub_tree(St,St)
-sub_tree(St,tree(X, left, right)) :- sub_tree() 
+sub_tree(T,T)
+sub_tree(T,tree(X, left, right)) :- sub_tree(T,left) 
+sub_tree(T,tree(X, left, right)) :- sub_tree(T,right) 
 
 
 
