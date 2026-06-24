9bfd38db5e9d46c006a444a10c4bf18ed2ef6113
ArielC0hen <arielguyc@gmail.com>
2026-06-24 23:55:48 +0300

    'autosave'

--- ppl/4/ex4/ex4.pl
+++ ppl/4/ex4/ex4.pl
@@ -44,7 +44,7 @@ sub_list(Xs,Ys) :- suffix(Rs, Ys), prefix(Xs, Rs).
 % Purpose: InversedList is the ‘mirror’ representation of List, i.e, each item in the list is recursively replaced with the item at the position, with refers to the beginning and the end of the list.   
 
 swap_list([],[]).
-swap_list([X|Xs],[Ys|X]) :- 
+swap_list([X|Xs],[Ys|X]) :- swap_list(Xs,Ys)
 
 
 
