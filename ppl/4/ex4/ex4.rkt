3466b5b465ba82819379c96161cbe46ac709f3c5
ArielC0hen <arielguyc@gmail.com>
2026-06-24 18:17:01 +0300

    'autosave'

--- ppl/4/ex4/ex4.rkt
+++ ppl/4/ex4/ex4.rkt
@@ -89,7 +89,8 @@
 ;;Tests: (get-value '((a . 3) (b . 4)) 'b) --> 4,(get-value '((a . 3) (b . 4)) 'c) --> 'fail
 (define get-value
   (lambda (assoc-list key)
-    if (eq? key (car assoc-list))
+    (if (eq? key (car assoc-list))
+    )
   )
 )
 
