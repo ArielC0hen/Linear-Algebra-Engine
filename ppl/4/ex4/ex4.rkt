334bab9ff63c3cd12c03021c20c9edacfe232feb
ArielC0hen <arielguyc@gmail.com>
2026-06-28 23:36:39 +0300

    'autosave'

--- ppl/4/ex4/ex4.rkt
+++ ppl/4/ex4/ex4.rkt
@@ -183,7 +183,19 @@
    
 
 ; bonus
+
 ; interleave:
+; a1,a2,a3,a4,... b1,b2,b3,b4,... -> a1,b1,a2,b2,a3,b3,a4,b4,...
+(define interleave
+  (lambda (lz1 lz2)
+    (if (empty-lzl? lz1)
+      lz2
+      (cons (car lz1) (interleave lz2 (tail lz2)))
+    )
+  )
+)
+
+; append:
 ; a1,a2,a3,a4,... b1,b2,b3,b4,... -> a1,b1,a2,b2,a3,b3,a4,b4,...
 (define interleave
   (lambda (lz1 lz2)
